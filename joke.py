85770d1c3562f87854f5c14fa0e93711cde6ab18
rval13v <livarfpv@gmail.com>
2025-10-12 22:51:10 +1100

    ddd

---
+++ joke.py
@@ -0,0 +1,19 @@
+# импортируем библиотеку
+import requests
+import json
+# Адрес на который будем отправлять запрос
+url = "https://official-joke-api.appspot.com/jokes/random"
+# Выводит на печать URL,по которому отправляем запрос
+print(url)
+# Создали переменную резалт и присвоили ей запрос с методом get на указанный url
+result = requests.get(url)
+# Выводит на печать статус-код ответа
+print("Статус код: " + str(result.status_code))
+# Проверяем ОР с ФР
+assert 200 == result.status_code
+if result.status_code == 200:
+    print('Cтатус код верен!')
+else:
+    print('Cтатус код не верен!')
+result.encoding = 'utf-8'
+print(result.json())
